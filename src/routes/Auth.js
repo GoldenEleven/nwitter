87ab1acb371464c8085d37b870269d324d4450ea
GoldenEleven <goldeneleven@outlook.kr>
2020-10-12 23:17:34 +0900

    Social Login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { authService } from "../fbase";
+import { authService, firebaseInstance } from "fbase";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
@@ -7,7 +7,7 @@ const Auth = () => {
     const [newAccount, setNewAccount] = useState(true);
     const [error, setError] = useState("");
     const onChange = (event) => {
-        const {target: {name, value}} = event;
+        const { target: { name, value } } = event;
         if (name === "email") {
             setEmail(value);
         } else if (name === "password") {
@@ -24,24 +24,36 @@ const Auth = () => {
                 data = await authService.signInWithEmailAndPassword(email, password)
             }
             console.log(data);
-        } catch(error) {
+        } catch (error) {
             setError(error.message);
-        }       
+        }
     };
     const toggleAccount = () => setNewAccount((prev) => !prev);
+    const onSocialClick = async (event) => {
+        const { target: { name } } = event;
+        let provider;
+        if (name === "google") {
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        } else if (name === "facebook") {
+            //provider = new firebaseInstance.auth.FacebookAuthProvider();
+        }
+        const data = await authService.signInWithPopup(provider);
+        console.log(data);
+
+    }
 
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} /> 
-                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} /> 
+                <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
+                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
                 <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
                 {error}
             </form>
-    <span onClick={toggleAccount}>{newAccount ? "Log In" : "Create Account"}</span>
+            <span onClick={toggleAccount}>{newAccount ? "Log In" : "Create Account"}</span>
             <div>
-                <button>Continue with Google</button>
-                <button>Continue with Facebook</button>
+                <button onClick={onSocialClick} name="google">Continue with Google</button>
+                <button onClick={onSocialClick} name="facebook">Continue with Facebook</button>
             </div>
         </div>
     )
